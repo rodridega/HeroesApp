4f725b16f9917c44ef48842b9be713e54e4487c2
rodridega <56453875+rodridega@users.noreply.github.com>
2022-04-05 15:45:51 -0300

    usando reducer y context

--- src/components/search/SearchScreen.js
+++ src/components/search/SearchScreen.js
@@ -1,7 +1,69 @@
-import React from 'react'
+import React, { useMemo } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import queryString from "query-string";
+import { heroes } from "../../data/heroes";
+import { useForm } from "../../hooks/useForm";
+import { getHeroesByName } from "../../selectors/getHeroesByName";
+import { HeroCard } from "../hero/HeroCard";
 
 export const SearchScreen = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const { q = "" } = queryString.parse(location.search);
+
+  const [values, handleInputChange] = useForm({
+    searchHeroe: q,
+  });
+
+  const { searchHeroe } = values;
+  const heroesFiltrados = useMemo(() => getHeroesByName(q), [q]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate(`?q=${searchHeroe}`);
+  };
+
   return (
-    <div>SearchScreen</div>
-  )
-}
+    <div className=" container row my-3">
+      <div className="col-5">
+        <h2>Buscar Héroe</h2>
+        <hr />
+        <form className="form-control">
+          <input
+            type="text"
+            placeholder="Nombre del Héroe"
+            name="searchHeroe"
+            autoComplete="off"
+            className="form-control"
+            onChange={handleInputChange}
+            value={searchHeroe}
+          />
+          <button
+            type="submit"
+            className="btn w-100 btn-outline-info mt-2"
+            onClick={handleSubmit}
+          >
+            Buscar
+          </button>
+        </form>
+      </div>
+      <div className="col-5">
+        <h2>Resultados</h2>
+        <hr />
+        {q === "" ? (
+          <p className=" fs-4 alert alert-info"> Busque un Héroe </p>
+        ) : (
+          heroesFiltrados.length === 0 && (
+            <div className="alert alert-danger fs-4">
+              `No hay resultados: {q} `
+            </div>
+          )
+        )}
+        {heroesFiltrados.map((hero) => (
+          <HeroCard key={hero.id} {...hero} />
+        ))}
+      </div>
+    </div>
+  );
+};
--- src/components/ui/NavBar.js
+++ src/components/ui/NavBar.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link, NavLink, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../auth/authContext";
 
 export const Navbar = () => {
+  const { user } = useContext(AuthContext);
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const handleLogOut = () => {
-    navigate('/login',{
-      replace: true
-    })
+    navigate("/login", {
+      replace: true,
+    });
   };
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
@@ -17,23 +19,40 @@ export const Navbar = () => {
 
       <div className="navbar-collapse">
         <div className="navbar-nav">
-          <NavLink className={({isActive}) => "nav-item nav-link" + (isActive ? ' active' : '')}    to="/marvel">
+          <NavLink
+            className={({ isActive }) =>
+              "nav-item nav-link" + (isActive ? " active" : "")
+            }
+            to="/marvel"
+          >
             Marvel
           </NavLink>
 
-          <NavLink className={({isActive}) => "nav-item nav-link" + (isActive ? ' active' : '')}   to="/dc">
+          <NavLink
+            className={({ isActive }) =>
+              "nav-item nav-link" + (isActive ? " active" : "")
+            }
+            to="/dc"
+          >
             DC
+          </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              "nav-item nav-link" + (isActive ? " active" : "")
+            }
+            to="/search"
+          >
+            Buscar
           </NavLink>
         </div>
       </div>
 
       <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
         <ul className="navbar-nav ml-auto">
-          <span className="nav-item nav-link text-info">Rodrigo</span>
+          <span className="nav-item nav-link text-info">{user.name}</span>
 
           <button
             className="nav-item nav-link btn"
-            
             to="/login"
             onClick={handleLogOut}
           >
